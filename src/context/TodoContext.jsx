fe92ef433986017e005d6ca2313b77512a19cdeb
mohitdheer <38219975+mohitdheer@users.noreply.github.com>
2023-08-19 18:21:51 +0530

    todo_app

--- src/context/TodoContext.jsx
+++ src/context/TodoContext.jsx
@@ -4,8 +4,8 @@ import { createContext, useContext, useEffect, useState } from "react";
 const TodoContext = createContext();
 
 export const TodoProvider = ({ children }) => {
-  const Todo_URL = "http://localhost:8080/todo_list";
-  // const Todo_URL = "https://todo-backend-2irx.onrender.com/todo_list";
+  // const Todo_URL = "http://localhost:8080/todo_list";
+  const Todo_URL = "https://todo-backend-2irx.onrender.com/todo_list";
   const [todoData, setTodoData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
